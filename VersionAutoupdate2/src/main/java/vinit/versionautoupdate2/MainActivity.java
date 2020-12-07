8c4fdb61e254135469df02f17ee2df26ca28565c
vinit <vinitkr28@gmail.com>
2020-12-07 12:44:52 +0530

    Initial commit

---
+++ VersionAutoupdate2/src/main/java/vinit/versionautoupdate2/MainActivity.java
@@ -0,0 +1,24 @@
+package vinit.versionautoupdate2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView1);
+
+        PackageManager packageManager = getPackageManager();
+
+        textView.setText("VERSION_NAME: " + BuildConfig.VERSION_NAME
+                + "\nVERSION_CODE: " + BuildConfig.VERSION_CODE
+                + "\nBUILD_TYPE: " + BuildConfig.BUILD_TYPE);
+    }
+}
---
+++ VersionAutoupdate5/src/main/java/vinit/versionautoupdate5/MainActivity.java
@@ -0,0 +1,31 @@
+package vinit.versionautoupdate5;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView1);
+
+        PackageManager packageManager = getPackageManager();
+
+        textView.setText("VERSION_NAME: " + BuildConfig.VERSION_NAME
+                + "\nVERSION_CODE: " + BuildConfig.VERSION_CODE
+                + "\nBUILD_TYPE: " + BuildConfig.BUILD_TYPE
+                + "\nAPPLICATION_ID: " + BuildConfig.APPLICATION_ID
+                + "\nAPK_FILE_NAME: " + BuildConfig.APK_FILE_NAME);
+
+        Log.d(TAG, "onCreate: " + textView.getText().toString());
+    }
+}
